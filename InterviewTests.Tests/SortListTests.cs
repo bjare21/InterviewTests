b5b740d4a78052901c7d82819b3d00987c4c24fd
bjare21 <jarek.barecki@gmail.com>
2022-04-04 23:27:30 +0200

    BubbleSort Algorithm Added

---
+++ InterviewTests.Tests/SortListTests.cs
@@ -0,0 +1,31 @@
+﻿using Listy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTests.Tests
+{
+    [TestClass]
+    public class SortListTests
+    {
+        [TestMethod]
+        public void BubbleSortTable()
+        {
+            //Arrange
+            int[] table = new int[10]
+            {
+                21,34,2,8,14,32,31,1,27,19
+            };
+
+            //Act
+            SortList.BubbleSort(table, 10);
+
+            //Assert
+            Assert.AreEqual(table[0], 1);
+            Assert.AreEqual(table[9], 34);
+        }
+    }
+}
---
+++ Listy/SortList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listy
+{
+    public static class SortList
+    {
+        public static void BubbleSort(int[] tab, int count)
+        {
+            int pom;
+            for (int i = count - 1; i > 0; i--)
+            {
+                for (int j=0; j<i; j++)
+                {
+                    if (tab[j] > tab[j + 1])
+                    {
+                        pom = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = pom;
+                    }
+                }
+            }
+        }
+    }
+}
